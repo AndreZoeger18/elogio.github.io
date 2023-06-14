29d819a9dc4f351fbdbe806c59f8f68c2bbf63bd
AndreZoeger18 <129678576+AndreZoeger18@users.noreply.github.com>
2023-06-14 09:50:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const horas = document.getElementsById('horas');
+const minutos = document.getElementsById('minutos');
+const segundos = document.getElementsById('segundos');
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr;
+
+    if (min < 10) min = '0' + min;
+
+    if (s < 10) s = '0' + s;
+
+    horas.textcontent = hr;
+    minutos.textcontent = min;
+    segundos.textcontent = s;
+})
